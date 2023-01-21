921b877bd98056a25e371554d02523d3cec38afb
Guramrit Pal Singh <77382202+amrit5s2002@users.noreply.github.com>
2023-01-21 20:35:45 +0530

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,12 +1,12 @@
 from base import swap,test_arrays
 
-# def bubble(array):
-#     for j in range(len(array)):
-#         for i in range(len(array)-1):
-#             if array[i]<array[i+1]:
-#                 array[i],array[i+1] = swap(array[i],array[i+1])
-#     return array
+def bubble(array):
+    for j in range(len(array)):
+        for i in range(len(array)-1):
+            if array[i]<array[i+1]:
+                array[i],array[i+1] = swap(array[i],array[i+1])
+    return array
 
-# array = test_arrays()
-# print('test_array :',array)
-# print(bubble(array))
+array = test_arrays()
+print('test_array :',array)
+print(bubble(array))
